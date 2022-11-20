c9fd6fdd44301ec815b38f60b9c829458b2ac4ee
thepedazomaster <Bryan_E_H@hotmail.com>
2022-11-20 18:41:15 -0500

    env añadido

--- database/database.js
+++ database/database.js
@@ -1,6 +1,11 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("administrador_cursos", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+export const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: MYSQLHOST,
+    dialect: "mysql",
+  }
+);
