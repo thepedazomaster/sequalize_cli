0620c347c49e9051896ee403300fb5a697fcd918
thepedazomaster <Bryan_E_H@hotmail.com>
2022-11-20 18:49:31 -0500

    arreglo 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // view engine setup
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(alumnosCursosRoutes);
